b8a8b2c2e7157b4e9ee8e3fec1c822d0f42baa66
bitoiu <vmrmonteiro@gmail.com>
2013-09-30 22:43:21 +0100

    how changing a readme file breaks everything - BAD KARMA! honestly documentation sucks balls regarding the changes in 0.10, documentation for requirejs usage is completely out of date.

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,61 @@
-// Karma configuration
-// Generated on Thu Jun 27 2013 19:27:55 GMT+0100 (BST)
+module.exports = function (config) {
+  config.set({
+
+    basePath : '',
+
+    // list of files / patterns to load in the browser
+    frameworks: ['mocha', 'requirejs'],
+    files : [
+      {pattern: 'node_modules/chai/chai.js', included: false},
+      {pattern: 'node_modules/lodash/lodash.js', included: false},
+      {pattern: 'test/*test*.js', included: false},
+      {pattern: 'venn.js', included: false},
+
+      'karma-require-conf.js'
+    ],
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // list of files to exclude
+    exclude : [],
 
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
-
-  {pattern: 'node_modules/chai/chai.js', included: false},
-  {pattern: 'node_modules/lodash/lodash.js', included: false},
-  {pattern: 'test/*test*.js', included: false},
-  {pattern: 'venn.js', included: false},
-
-  'karma-require-conf.js'
-];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    reporters : ['progress'],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // web server port
+    port : 9876,
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // cli runner port
+    runnerPort : 9100,
 
 
-// web server port
-port = 9876;
+    // enable / disable colors in the output (reporters and logs)
+    colors : true,
 
 
-// cli runner port
-runnerPort = 9100;
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel : config.LOG_INFO,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch : true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // Start these browsers, currently available:
+    browsers : ['Chrome'],
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout : 60000,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun : false
 
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+  });
+};
